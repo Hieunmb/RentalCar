2c63646cd0f3c148e74024b3e1847d2aacff2422
Hieunmb <zhuongdae@gmail.com>
2023-10-31 23:20:06 +0700

    brand controller

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentalCar.DTOs;
+using RentalCar.Entities;
+
+namespace RentalCar.Controllers
+{
+    [Route("/brand")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly RentalCarContext _context;
+        public BrandController(RentalCarContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Brand> brands = _context.Brands.ToList();
+            List<BrandDTO> data= new List<BrandDTO>();
+            foreach (Brand brand in brands)
+            {
+                data.Add(new BrandDTO { id=brand.Id, name=brand.Name,icon=brand.Icon});
+            }
+            return Ok(data);
+        }
+    }
+}
---
+++ DTOs/BrandDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RentalCar.DTOs
+{
+    public class BrandDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string icon { get; set; }
+    }
+}
